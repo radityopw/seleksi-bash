4c9a5a064e60ad373a6d8acb646186ec75609b1b
radityopw <radityo_pw@is.its.ac.id>
2022-07-14 21:01:24 +0700

    penambahan tabel peserta_diterima

---
+++ run.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+
+## input 
+## input 1 : data penempatan
+## input 2 : data peserta
+
+## preparasi data
+## 1. buat folder temporary
+## 2. copy database.db ke point 1 -> database_aktif.db
+
+## import data ke dalam database
+## 1. import data penempatan ke database_aktif.db
+## 2. import data peserta ke database_aktif.db
+## jika terdapat error maka system halted
+
+## proses seleksi 
+## 1.deteksi ada berapa pilihan maksimal pada data peserta 
+## 2.buat fungsi yang melakukan seleksi pilihan 1
+## 3.buat fungsi yang melakukan seleksi pilihan 2 dst ( looping sampai tidak ada lagi yang eligible )
+
+
+## output list nomor peserta dan kode penempatan 
+
