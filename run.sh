ade15204c35c5ea5088afeaf9872dd5860730b8c
radityo prasetianto wibowo <radityo_pw@is.its.ac.id>
2022-07-15 21:36:54 +0700

    modul import data

--- run.sh
+++ run.sh
@@ -7,10 +7,12 @@ set -e
 # echo an error message before exiting
 #trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
+#force working directory on this file
+cd "$(dirname "$0")"
+
+
 
 ## functions
-
-
 help(){
 
 	# Display Help
@@ -29,6 +31,8 @@ help(){
 data_penempatan=""
 data_peserta=""
 number_params=0
+database=""
+max_pil=0
 
 ## input 
 ## input 1 : data penempatan
@@ -38,10 +42,12 @@ while getopts ":h" option; do
 	case $option in
 		h) # display Help
 			help
-			exit;;
+			exit
+			;;
 		\?) # invalid options
 			help
-			exit;;
+			exit
+			;;
 	esac
 done
 
@@ -57,13 +63,21 @@ else
 fi
 
 ## preparasi data
-## 1. buat folder temporary
+## 1. buat temporary file
 ## 2. copy database.db ke point 1 -> database_aktif.db
+database=$(mktemp)
+cp database.db $database
+
 
 ## import data ke dalam database
 ## 1. import data penempatan ke database_aktif.db
 ## 2. import data peserta ke database_aktif.db
 ## jika terdapat error maka system halted
+
+sqlite3 $database "DELETE FROM peserta_diterima; DELETE FROM peserta; DELETE FROM penempatan;"
+
+sqlite3 $database ".import --csv --skip 1 "$data_penempatan" penempatan"
+sqlite3 $database ".import --csv --skip 1 "$data_peserta" peserta"
 
 ## proses seleksi 
 ## 1.deteksi ada berapa pilihan maksimal pada data peserta 
@@ -71,5 +85,10 @@ fi
 ## 3.buat fungsi yang melakukan seleksi pilihan 2 dst ( looping sampai tidak ada lagi yang eligible )
 
 
+
+
+
 ## output list nomor peserta dan kode penempatan 
 
+
+exit 0
